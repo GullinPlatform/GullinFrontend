b54f4c8c3ab8c3754372d9b1e6d2d90e4869cc06
C0deZ Lee <c0dezlee@gmail.com>
2018-02-04 18:25:17 -0500

    Utilize Etherscan API

--- src/store/modules/wallet.js
+++ src/store/modules/wallet.js
@@ -1,4 +1,5 @@
 import walletApi from '../../api/wallet-api'
+import etherscanApi from '../../api/etherscan-api'
 import * as types from '../mutation-types'
 
 import { web3, erc20_contract_abi } from '../../utils'
@@ -26,29 +27,71 @@ const actions = {
       })
       .catch((error) => Promise.reject(error))
   },
+  getTransactions({ commit }) {
+    return walletApi.getWallet()
+      .then((response) => {
+        commit(types.LOAD_TRANSACTIONS, response)
+        return Promise.resolve(response)
+      })
+      .catch((error) => Promise.reject(error))
+  },
+  addTransaction({ commit }, form_data) {
+    return walletApi.addTransaction(form_data)
+      .then((response) => {
+        commit(types.LOAD_TRANSACTIONS, response)
+        return Promise.resolve(response)
+      })
+      .catch((error) => Promise.reject(error))
+  },
+
+  // Get balance
   syncEthBalance({ state, commit, dispatch }) {
-    if (state.wallet.eth_address) {
-      web3.eth.getBalance(state.wallet.eth_address)
-        .then((eth_balance) => {
-          const form_data = {
-            ETH: eth_balance,
-          }
-          walletApi.syncBalance(form_data)
-            .then(() => {
-              commit(types.SYNC_BALANCE)
-              dispatch('getWallet')
-            })
-        })
+    etherscanApi.getETHBalanceByAddress(state.wallet.eth_address)
+      .then((response) => {
+        const form_data = {
+          ETH: response.result,
+        }
+        walletApi.syncBalance(form_data)
+          .then(() => {
+            commit(types.SYNC_BALANCE)
+            dispatch('getWallet')
+          })
+      })
+  },
+  syncTokenBalance({ state, commit, dispatch }) {
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+    // For every token balance in wallet record
+    for (const token_balance of state.wallet.balances) {
+      // If the token balance has token_address (aka is not ETH)
+      if (token_balance.token_address) {
+        // Get balance from etherscan api
+        etherscanApi.getTokenBalanceByAddress(state.wallet.eth_address, token_balance.token_address)
+          .then((response) => {
+            const form_data = {}
+            form_data[token_balance.token_code] = response.result
+            walletApi.syncBalance(form_data)
+              .then(() => {
+                commit(types.SYNC_BALANCE)
+                dispatch('getWallet')
+              })
+          })
+      }
+      // Sleep to avoid exceed max call/sec
+      sleep(500)
     }
   },
-  sendEth({ commit }, form_data) {
-    web3.eth.accounts.signTransaction({
-      to: form_data.to_address,
-      value: web3.utils.toWei(form_data.value, 'ether'),
+
+  // Send tokens
+  sendEth({ state, commit }, data) {
+    const raw_transaction = {
+      from: state.wallet.eth_address,
+      to: data.to_address,
+      value: data.value,
       gas: 2000000,
-    }, form_data.private_key)
+    }
+    web3.eth.accounts.signTransaction(raw_transaction, data.private_key)
       .then((signed_transaction) => {
-        web3.eth.sendSignedTransaction('0x' + signed_transaction.rawTransaction)
+        web3.eth.sendSignedTransaction(signed_transaction.rawTransaction)
           .on('transactionHash', hash => {
             console.log(hash)
           })
@@ -61,16 +104,19 @@ const actions = {
           .on('error', console.error);
       })
   },
-  sendToken({ commit }, form_data) {
-    const contract = web3.eth.Contract(erc20_contract_abi).at(form_data.contract_address)
-    const encoded_abi_data = contract.methods.transfer(form_data.to_address, form_data.value).encodeABI()
-    web3.eth.accounts.signTransaction({
-      to: form_data.contract_address,
-      data: encoded_abi_data,
+  sendToken({ commit }, data) {
+    const contract = web3.eth.Contract(erc20_contract_abi).at(data.contract_address)
+    const abi_data = contract.methods.transfer(data.contract_address, data.value, { from: data.from_address }).encodeABI()
+    const raw_transaction = {
+      from: state.wallet.eth_address,
+      to: data.contract_address,
+      data: abi_data,
       gas: 2000000,
-    }, form_data.private_key)
+    }
+
+    web3.eth.accounts.signTransaction(raw_transaction, data.private_key)
       .then((signed_transaction) => {
-        web3.eth.sendSignedTransaction('0x' + signed_transaction.rawTransaction)
+        web3.eth.sendSignedTransaction(signed_transaction.rawTransaction)
           .on('transactionHash', hash => {
             console.log(hash)
           })
@@ -82,6 +128,43 @@ const actions = {
           })
           .on('error', console.error);
       })
+  },
+
+  // Get Transactions
+  syncEthTransactions({ state, commit, dispatch }) {
+    etherscanApi.getETHTransactionsByAddress(state.wallet.eth_address)
+      .then((response) => {
+        const form_data = {
+          ETH: response.result,
+        }
+        walletApi.syncBalance(form_data)
+          .then(() => {
+            commit(types.SYNC_BALANCE)
+            dispatch('getWallet')
+          })
+      })
+  },
+  syncTokenTransactions({ state, commit, dispatch }) {
+    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+    // For every token balance in wallet record
+    for (const token_balance of state.wallet.balances) {
+      // If the token balance has token_address (aka is not ETH)
+      if (token_balance.token_address) {
+        // Get balance from etherscan api
+        etherscanApi.getTokenTransactionsByAddress(state.wallet.eth_address, token_balance.token_address)
+          .then((response) => {
+            const form_data = {}
+            form_data[token_balance.token_code] = response.result
+            walletApi.syncBalance(form_data)
+              .then(() => {
+                commit(types.SYNC_BALANCE)
+                dispatch('getWallet')
+              })
+          })
+      }
+      // Sleep to avoid exceed max call/sec
+      sleep(500)
+    }
   },
 }
 
