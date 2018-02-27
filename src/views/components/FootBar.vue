5a9023bc97789ecb385e5df1896b9091c9ae1d28
C0deZ Lee <c0dezlee@gmail.com>
2018-02-27 01:17:16 -0500

    Add risk disclaimer on footer

--- src/views/components/FootBar.vue
+++ src/views/components/FootBar.vue
@@ -1,10 +1,13 @@
 <template>
   <!-- begin::Footer -->
   <footer class="footer">
-    <div class="container">
+    <div class="container-fluid">
       <div class="row">
-        <div class="col-12 text-center">
+        <div class="col-6">
           2018 © Gullin LLC
+        </div>
+        <div class="col-6 text-right">
+          <a href="https://gullin.io/risk.html" target="_blank"> Risk Disclaimer</a>
         </div>
       </div>
     </div>
